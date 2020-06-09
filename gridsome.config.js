50b6da33e8da8ab8dcaaae017bc1a6d8c78832b1
NavaneethVijay <NavaneethVijay>
2020-06-09 11:29:14 +0530

    first commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,6 +3,7 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+require('dotenv').config()
 const tailwind = require('tailwindcss')
 const purgecss = require('@fullhuman/postcss-purgecss')
 
@@ -17,11 +18,41 @@ module.exports = {
     {
       use: '@gridsome/source-contentful',
       options: {
-        space: '2ku4he7c4u1i', // required
-        accessToken: 'oaG5LXdBib5fdnJuR8NY_hBmvES9cgFfebNRPZhNeO8', // required
+        space: process.env.CONTENTFUL_SPACE, // required
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, // required
         host: 'cdn.contentful.com',
         environment: 'master',
         typeName: 'Contentful',
+      },
+    },
+    {
+      use: 'gridsome-plugin-manifest',
+      options: {
+        background_color: '##000810',
+        icon_path: './src/favicon.png',
+        name: 'NavaneethVijay',
+        short_name: 'NavaneethVijay',
+        theme_color: '#961029',
+        lang: 'en',
+      },
+    },
+    {
+      use: 'gridsome-plugin-pwa',
+      options: {
+        title: 'NavaneethVijay',
+        startUrl: '/',
+        display: 'standalone',
+        statusBarStyle: 'default',
+        manifestPath: 'manifest.json',
+        disableServiceWorker: true,
+        serviceWorkerPath: 'service-worker.js',
+        cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+        shortName: 'NavaneethVijay',
+        themeColor: '#666600',
+        backgroundColor: '#ffffff',
+        icon: 'src/favicon.png', // must be provided like 'src/favicon.png'
+        msTileImage: '',
+        msTileColor: '#666600',
       },
     },
   ],
