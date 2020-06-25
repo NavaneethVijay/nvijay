dd1e3baf84ba6d1385ab8d1d442598f1fc5cd32f
NavaneethVijay <NavaneethVijay>
2020-06-25 19:35:10 +0530

    updated about me and changes

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -34,14 +34,6 @@
           </div>
 
           <div class="my-8">
-            <h3 class="text-lg md:text-xl font-regular">
-              I've been Working as a
-              <span class="font-medium text-xl md:text-2xl">
-                Magento Frontend Developer </span
-              >in Codilar Technologies Pvt. Ltd in Bengaluru for 2 year and
-              above with active contributations in development for more than 5
-              projects.
-            </h3>
             <h3 class="text-lg md:text-xl font-regular my-4">
               While I binge watch tv series and play
               <span class="font-medium text-xl md:text-2xl"> Dota2</span> on a
@@ -78,13 +70,13 @@
               </div>
             </div>
             <div>
-              <div class="md:px-6 py-5 md:py-10 rounded">
-                <div class="flex">
+              <div class="md:pr-6 py-5 md:py-10 rounded ">
+                <div class="flex items-start">
                   <img
                     class="w-16 md:w-20 h-16 md:h-20 cover"
                     src="https://pbs.twimg.com/profile_images/822422940377554944/Hk7sgQ96_400x400.jpg"
                   />
-                  <div class="pl-4">
+                  <div class="pl-4 relative">
                     <h5 class="font-medium text-lg md:text-2xl">
                       Magento Frontend Developer
                     </h5>
@@ -92,13 +84,37 @@
                       Codilar Technologies Pvt. Ltd
                     </p>
                     <p class="text-sm text-gray-700">June 2018 - Current</p>
+                    <div class="absolute bottom-0 right-0 opacity-25">
+                      <span class="font-bold text-3xl font-serif opacity-25"
+                        >02</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="md:pr-6 py-5 md:py-10 rounded ">
+                <div class="flex items-start">
+                  <div class="w-16 md:w-20 h-16 md:h-20 cover bg-gray-200" />
+                  <div class="pl-4 relative">
+                    <h5 class="font-medium text-lg md:text-2xl">
+                      Freelance Graphic Designer
+                    </h5>
+                    <p class="text-sm md:text-base">
+                      Self Employed
+                    </p>
+                    <p class="text-sm text-gray-700">Feb 2017 - June 2018</p>
+                    <div class="absolute bottom-0 right-0 opacity-25">
+                      <span class="font-bold text-3xl font-serif opacity-25"
+                        >01</span
+                      >
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="overflow-hidden  relative mb-20">
+          <div class="overflow-hidden  relative ">
             <div class="container mx-auto rounded-lg py-6">
               <div class="w-full">
                 <div class="md:pb-5">
@@ -108,7 +124,6 @@
                         Arsenal kit
                       </h3>
                     </div>
-                    <div class="w-32 headling-line hero-side-bg m-0"></div>
                   </div>
                   <ul class="mt-10 text-xl font-medium flex flex-wrap">
                     <li class="pr-10 mb-6">HTML</li>
@@ -120,6 +135,53 @@
                     <li class="pr-10 mb-6">Laravel</li>
                     <li class="pr-10 mb-6">MySQL</li>
                   </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mt-10">
+            <div class="pb-5 md:px-0 md:pt-10">
+              <h3 class="text-2xl md:text-4xl font-semibold mb-4 ">
+                Education
+              </h3>
+            </div>
+            <div>
+              <div class="md:pr-6 py-5 md:py-10 rounded ">
+                <div class="flex items-start">
+                  <div class="relative">
+                    <h5 class="font-medium text-lg md:text-2xl">
+                      B.Tech in Engineering (ISE)
+                    </h5>
+                    <p class="text-sm md:text-base">
+                      BNM Institute of Technology
+                    </p>
+                    <p class="text-sm text-gray-700">2014 - 2018</p>
+                    <div class="absolute bottom-0 right-0 opacity-25">
+                      <span class="font-bold text-3xl font-serif opacity-25"
+                        >04</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="md:pr-6 py-5 md:py-10 rounded ">
+                <div class="flex items-start">
+                  <div class="relative">
+                    <h5 class="font-medium text-lg md:text-2xl">
+                      Pre University of Karnataka
+                    </h5>
+                    <p class="text-sm md:text-base">
+                      Christ University, Bengaluru
+                    </p>
+                    <p class="text-sm text-gray-700">2012 - 2014</p>
+                    <div class="absolute bottom-0 right-0 opacity-25">
+                      <span class="font-bold text-3xl font-serif opacity-25"
+                        >02</span
+                      >
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
